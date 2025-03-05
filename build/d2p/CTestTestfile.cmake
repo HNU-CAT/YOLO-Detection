b5b491b503d2c513115cd671837465fefd8098cc
liuxingxun <107916318+XXLiu-HNU@users.noreply.github.com>
2025-03-05 23:09:03 +0800

    v2

--- build/d2p/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xingxun/experment/Depth2Pose/src/d2p
-# Build directory: /home/xingxun/experment/Depth2Pose/build/d2p
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/d2p/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/include/eigen3".split(';') if "/usr/include/eigen3" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;sensor_msgs;geometry_msgs;nav_msgs;visualization_msgs;vision_msgs;cv_bridge;image_transport;pcl_ros;pcl_conversions".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "d2p"
-PROJECT_SPACE_DIR = "/home/xingxun/experment/Depth2Pose/install"
-PROJECT_VERSION = "0.0.1"
